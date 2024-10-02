4b94a109644da717cf2dc01f55249b1bd773ebda
krishundre <krish.undre@somaiya.edu>
2024-10-02 20:00:23 +0530

    Updated link in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
       <div className="container">
         <div className="row footer-top">
           {/* Logo Section */}
-           <div className="col-md-5 d-flex justify-content-center align-items-center mb-3 mb-md-0">
-           <a className="navbar-brand" href="/">
-            <img src="https://via.placeholder.com/150x50?text=Invoiceji" alt="Invoiceji Logo" className='mx-auto my-auto invoiceji-logo'/>
+          <div className="col-md-5 d-flex justify-content-center align-items-center mb-3 mb-md-0">
+            <a className="navbar-brand" href="/">
+              <img src="https://via.placeholder.com/150x50?text=Invoiceji" alt="Invoiceji Logo" className='mx-auto my-auto invoiceji-logo' />
             </a>
           </div>
 
@@ -37,7 +37,7 @@ const Footer = () => {
               <li><a href="https://krishundre.vercel.app/" target='_blank' rel='noreferrer'>About Developer</a></li>
             </ul>
           </div>
-          
+
           {/* Other Projects Section */}
           <div className="col-md-2 footer-project">
             <h5 className='text-decoration-underline'>Other Projects</h5>
@@ -46,8 +46,8 @@ const Footer = () => {
               <li><a href="https://brzee.shop/" target='_blank' rel='noreferrer'>Brzee Jewelry</a></li>
               <li><a href="https://linkers-project.vercel.app/" target='_blank' rel='noreferrer'>Linkers' Project</a></li>
               <li><a href="https://teraibazaar.com/" target='_blank' rel='noreferrer'>Terai Bazaar</a></li>
-              <li><a href="https://medical-directory.vercel.app/" target='_blank' rel='noreferrer'>Online Medical Directory</a></li>
-              <li><a href="https://www.boutiquesofindia.in/" target='_blank' rel='noreferrer'>Boutiques of India</a></li>
+              <li><a href="https://krishundre.vercel.app/projects" target='_blank' rel='noreferrer'>View all Projects</a></li>
+
             </ul>
           </div>
         </div>
