76a592c3e34d67d0ce767206fa5e5cea6488e3eb
krishundre <krish.undre@somaiya.edu>
2024-10-02 19:57:15 +0530

    Added Download Templates Page to Project and its link to the footer page with new upate

--- src/Components/DownloadTemplates.jsx
+++ src/Components/DownloadTemplates.jsx
@@ -5,7 +5,7 @@ import { LuHardDriveDownload } from "react-icons/lu";
 const DownloadTemplates = () => {
   return (
     <div className="container-fluid download-templates px-5">
-      <h2 className="text-center section-heading">Download Templates</h2>
+      <h2 className="text-center section-heading"><bold>Download Templates</bold></h2>
       <div className="row align-items-center template-item">
         <div className="col-md-6">
           <img src="https://placehold.co/500x300" alt="Balance Sheet" className="img-fluid" />
@@ -14,7 +14,15 @@ const DownloadTemplates = () => {
           <h3 className="template-heading">Balance Sheet</h3>
           <hr />
           <p>Download our professionally designed balance sheet template to organize your financial data.</p>
-          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2'/></button>
+          <h5 className="download-title"><bold>Downloadable in:</bold></h5>
+
+          {/* List of download formats */}
+          <ul className="download-formats">
+            <li>PDF</li>
+            <li>Google Sheets</li>
+            <li>Excel</li>
+            <li>Word</li>
+          </ul><button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2' /></button>
         </div>
       </div>
 
@@ -26,7 +34,16 @@ const DownloadTemplates = () => {
           <h3 className="template-heading">Profit & Loss Statement</h3>
           <hr />
           <p>Get a comprehensive Profit & Loss statement template to track your business performance.</p>
-          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2'/></button>
+          <h5 className="download-title"><bold>Downloadable in:</bold></h5>
+
+          {/* List of download formats */}
+          <ul className="download-formats">
+            <li>PDF</li>
+            <li>Google Sheets</li>
+            <li>Excel</li>
+            <li>Word</li>
+          </ul>
+          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2' /></button>
         </div>
       </div>
 
@@ -39,7 +56,17 @@ const DownloadTemplates = () => {
           <h3 className="template-heading">GST Bill Format</h3>
           <hr />
           <p>Download a GST-compliant bill format that ensures proper tax documentation.</p>
-          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2'/></button>
+          {/* Title for Download Formats */}
+          <h5 className="download-title"><bold>Downloadable in:</bold></h5>
+
+          {/* List of download formats */}
+          <ul className="download-formats">
+            <li>PDF</li>
+            <li>Google Sheets</li>
+            <li>Excel</li>
+            <li>Word</li>
+          </ul>
+          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2' /></button>
         </div>
       </div>
 
@@ -51,7 +78,17 @@ const DownloadTemplates = () => {
           <h3 className="template-heading">Commercial Invoice</h3>
           <hr />
           <p>Get a professional commercial invoice template suitable for international trade.</p>
-          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2'/></button>
+          {/* Title for Download Formats */}
+          <h5 className="download-title"><bold>Downloadable in:</bold></h5>
+
+          {/* List of download formats */}
+          <ul className="download-formats">
+            <li>PDF</li>
+            <li>Google Sheets</li>
+            <li>Excel</li>
+            <li>Word</li>
+          </ul>
+          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2' /></button>
         </div>
       </div>
 
@@ -63,7 +100,17 @@ const DownloadTemplates = () => {
           <h3 className="template-heading">Income Statement</h3>
           <hr />
           <p>Download an income statement template to calculate your profits and losses accurately.</p>
-          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2'/></button>
+          {/* Title for Download Formats */}
+          <h5 className="download-title"><bold>Downloadable in:</bold></h5>
+
+          {/* List of download formats */}
+          <ul className="download-formats">
+            <li>PDF</li>
+            <li>Google Sheets</li>
+            <li>Excel</li>
+            <li>Word</li>
+          </ul>
+          <button className="btn w-auto btn-primary download-btn d-flex align-items-center">Download <LuHardDriveDownload className='ms-2' /></button>
         </div>
       </div>
     </div>
