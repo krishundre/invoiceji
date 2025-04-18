870852b50f6ecdd7e59eaffff2fb184f25cbe3e8
krishundre <krish.undre@somaiya.edu>
2025-04-19 04:31:50 +0530

    `Refactor Footer component: adjust column widths and remove links section`

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -28,26 +28,18 @@ const Footer = () => {
           </div>
 
           {/* Extra Links Section */}
-          <div className="col-md-2 footer-extra">
+          <div className="col-md-3 footer-extra">
             <h5>Extra Links</h5>
             <ul>
               <li><a href="/documentation">Documentation</a></li>
               <li><a href="/faqpage">FAQ</a></li>
               <li><a href="/termsandserviecspage">Terms & Conditions</a></li>
-              <li><a href="https://krishundre.vercel.app/" target="_blank" rel="noreferrer">About Developer</a></li>
+              {/* <li><a href="https://krishundre.vercel.app/" target="_blank" rel="noreferrer">About Developer</a></li> */}
             </ul>
           </div>
 
           {/* Other Projects Section */}
-          <div className="col-md-2 footer-project">
-            <h5>Other Projects</h5>
-            <ul>
-              <li><a href="https://merlot.com/" target="_blank" rel="noreferrer">Merlot Trading</a></li>
-              <li><a href="https://brzee.shop/" target="_blank" rel="noreferrer">Brzee Jewelry</a></li>
-              <li><a href="https://linkers-project.vercel.app/" target="_blank" rel="noreferrer">Linkers' Project</a></li>
-              <li><a href="https://krishundre.vercel.app/projects" target="_blank" rel="noreferrer">View all Projects</a></li>
-            </ul>
-          </div>
+
         </div>
 
         {/* Second Strip */}
